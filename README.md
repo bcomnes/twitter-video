f1c2e61b75f9d181479c3aacfe384051ab2bdaff
Bret Comnes <bcomnes@gmail.com>
2015-07-22 17:54:57 -0700

    basic module working

---
+++ README.md
@@ -0,0 +1,31 @@
+# twitvid
+
+[![npm][npm-image]][npm-url]
+[![travis][travis-image]][travis-url]
+
+[npm-image]: https://img.shields.io/npm/v/twitvid.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/twitvid
+[travis-image]: https://img.shields.io/travis/bcomnes/twitvid.svg?style=flat-square
+[travis-url]: https://travis-ci.org/bcomnes/twitvid
+
+
+
+## Install
+
+```
+npm install twitvid
+```
+
+## Usage
+
+```js
+var twitvid = require('twitvid')
+```
+
+## Contributing
+
+Contributions welcome! Please read the [contributing guidelines](CONTRIBUTING.md) first.
+
+## License
+
+[ISC](LICENSE.md)
---
+++ index.js
@@ -0,0 +1,92 @@
+var request = require('request')
+var fs = require('fs')
+var Writable = require('stream').Writable
+var extend = require('util-extend')
+
+var ENDPOINT = 'https://upload.twitter.com/1.1/media/upload.json'
+
+function twitterVideo (path, oauth, cb) {
+  // Caution!
+  // https://dev.twitter.com/overview/api/twitter-ids-json-and-snowflake
+  var media_id
+  var oauthReq = {
+    consumer_key: oauth.consumer_key,
+    consumer_secret: oauth.consumer_secret,
+    token: oauth.access_token,
+    token_secret: oauth.access_token_secret
+  }
+
+  var baseReq = {
+    url: ENDPOINT,
+    oauth: oauthReq,
+    json: true
+  }
+
+  var initReq = extend({
+    formData: {
+      media_type: 'video/mp4',
+      command: 'INIT'
+    }
+  }, baseReq)
+
+  var finalReq = extend({
+    formData: {
+      command: 'FINALIZE'
+    }
+  }, baseReq)
+
+  fs.stat(path, function (err, stats) {
+    if (err) return cb(err)
+    initReq.formData.total_bytes = stats.size
+    request.post(initReq, function cb (err, res, body) {
+      debugger
+      if (err) return cb(err)
+      console.log(body)
+      media_id = body.media_id_string
+      finalReq.formData.media_id = media_id
+
+      console.log(finalReq)
+
+      var twitAppend = twitterUpload(media_id, baseReq)
+      var fileStream = fs.createReadStream(path)
+
+      twitAppend.on('error', function streamError (err) {
+        return cb(err)
+      })
+      twitAppend.on('finish', function finalize () {
+        request.post(finalReq, function (err, res, body) {
+          return cb(err, media_id)
+        })
+      })
+      fileStream.pipe(twitAppend)
+    })
+  })
+}
+
+function counter () {
+  var i = 0
+  return function aCounter () {
+    return i++
+  }
+}
+
+function twitterUpload (media_id, baseReq) {
+  var c = counter()
+  var append = Writable()
+  append._write = function write (chunk, enc, next) {
+    var appendReq = extend({ formData: {
+        segment_index: c(),
+        command: 'APPEND',
+        media_id: media_id,
+        media: chunk
+      }
+    }, baseReq)
+    console.log(appendReq)
+    request.post(appendReq, function append (err, res, body) {
+      next(err)
+    })
+  }
+  return append
+}
+
+module.exports = twitterVideo
